4360d670da548cc8cfb4d58482aa3b20672638ad
Wei Zhang <zw83133883@yahoo.com>
2018-04-12 01:41:20 -0400

    Created Login window, and connected to database.

--- src/databasegui/LoginController.java
+++ src/databasegui/LoginController.java
@@ -1,5 +1,49 @@
 package databasegui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import javax.xml.soap.Text;
+import java.awt.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+import java.awt.event.ActionEvent;
+
+
 public class LoginController {
+    @FXML
+    TextField usernameField,passwordField;
+    @FXML
+    public void login(javafx.event.ActionEvent event) throws ClassNotFoundException,InstantiationException, IllegalAccessException, SQLException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        Object newInstance;
+        newInstance = Class.forName("com.mysql.jdbc.Driver").newInstance();
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://triton.towson.edu:3360/wzhang9db", username, password);// Please use your database name here
+            PreparedStatement updateStaff;
+            Statement queryStatement = connection.createStatement();
+            updateStaff = null;
+            String querys = "select * from EMPLOYEE;";
+            ResultSet results = queryStatement.executeQuery(querys);
+            while (results.next()) {
+                System.out.print(results.getString("fName"));
+                System.out.print("    ");
+                System.out.print(results.getString("lName"));
+                System.out.println();
+            }
+        }
+        catch(Exception e){
+            System.out.println("Wrong Input");
+        }
+//        querys="UPDATE wzhang9db.EMPLOYEE SET salary = 8802 WHERE SSN = '123456789';";
+//        updateStaff = connection.prepareStatement(querys);
+
+    }
 
 }
--- src/databasegui/Main.java
+++ src/databasegui/Main.java
@@ -5,18 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.*;
+import java.util.*;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 500, 450));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
