c1a3b77a8637578fe5ac020206a4fdbef6360fb1
ylerxyz <tyler.smith803@gmail.com>
2018-04-13 15:26:02 -0400

    Data now listed in TableView, progress on updating data

--- src/databasegui/MainWindow/MainWindowController.java
+++ src/databasegui/MainWindow/MainWindowController.java
@@ -1,15 +1,21 @@
 package databasegui.MainWindow;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.Callback;
 
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable {
@@ -17,24 +23,53 @@ public class MainWindowController implements Initializable {
     @FXML
     TableView table;
 
+    private ObservableList<ObservableList> data = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        table.setEditable(true);
         try{
             Object newInstance = Class.forName("com.mysql.jdbc.Driver").newInstance();
             Connection connection = DriverManager.getConnection("jdbc:mysql://triton.towson.edu:3360/tsmith70db", "tsmith70", "Cosc*ey2h");
 
-            Statement queryStatement = connection.createStatement();
-            ResultSet columnResults = queryStatement.executeQuery("select * from INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'EMPLOYEE'");
             ResultSet results = connection.createStatement().executeQuery("select * from EMPLOYEE");
-            while (columnResults.next()) {
-                String columnName = columnResults.getString("COLUMN_NAME");
-                System.out.println(columnName);
-                while (results.next()){
-                    System.out.println(results.getString(columnName));
+            for(int i = 0; i < results.getMetaData().getColumnCount(); i++){
+                final int j = i;
+                String columnName = results.getMetaData().getColumnName(i+1);
+                TableColumn col = new TableColumn(columnName);
+                if(i+1 == 1){
+                    col.setEditable(false);
                 }
-                results.beforeFirst();
-                table.getColumns().add(new TableColumn<String, String>(columnName));
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+                col.setCellFactory(TextFieldTableCell.forTableColumn());
+                col.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent>() {
+                    @Override
+                    public void handle(TableColumn.CellEditEvent event) {
+                        String columnName = event.getTablePosition().getTableColumn().getText();
+                        
+                    }
+                });
+
+                table.getColumns().addAll(col);
             }
+
+            while(results.next()){
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i = 0; i < results.getMetaData().getColumnCount(); i++){
+                    if(results.getString(i+1) != null){
+                        row.add(results.getString(i+1));
+                    } else {
+                        row.add("");
+                    }
+                }
+                data.add(row);
+            }
+
+            table.setItems(data);
 
         } catch (Exception e) {
             System.out.println(e);
