1d5b736bbfe0d2f4e7a9d44ec4f0b24c66b9a8e8
ty-ler <tyler.smith803@gmail.com>
2018-04-12 20:59:57 -0400

    Added tabbed indexing

--- src/databasegui/LoginWindow/LoginController.java
+++ src/databasegui/LoginWindow/LoginController.java
@@ -2,24 +2,50 @@ package databasegui.LoginWindow;
 
 import databasegui.MainWindow.MainWindow;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
-import javax.xml.soap.Text;
-import java.awt.*;
+import java.net.URL;
+import java.security.Key;
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ResourceBundle;
 
-
-
-import java.awt.event.ActionEvent;
-
-
-public class LoginController {
+public class LoginController implements Initializable{
     @FXML
     TextField usernameField,passwordField;
+
     @FXML
-    public void login(javafx.event.ActionEvent event) throws ClassNotFoundException,InstantiationException, IllegalAccessException, SQLException {
+    Button loginButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        usernameField.setOnAction(event -> {
+
+        });
+
+        usernameField.addEventFilter(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+            if(event.getCode() == KeyCode.TAB){
+                passwordField.setFocusTraversable(true);
+            }
+        });
+
+        passwordField.addEventFilter(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+            if(event.getCode() == KeyCode.TAB && event.isShiftDown()){
+                usernameField.setFocusTraversable(true);
+            }
+        });
+    }
+
+    @FXML
+    public void loginAction(javafx.event.ActionEvent event) throws Exception{
+        login();
+    }
+
+    public void login() throws Exception{
         String username = usernameField.getText();
         String password = passwordField.getText();
 
@@ -32,14 +58,18 @@ public class LoginController {
             String querys = "select * from EMPLOYEE;";
             ResultSet results = queryStatement.executeQuery(querys);
 
-            MainWindow.launchApp();
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+
+            stage.close();
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.launch();
         }
         catch(Exception e){
-            System.out.println("Wrong Input");
+            System.out.println(e);
         }
 //        querys="UPDATE wzhang9db.EMPLOYEE SET salary = 8802 WHERE SSN = '123456789';";
 //        updateStaff = connection.prepareStatement(querys);
-        
     }
 
 }
--- src/databasegui/MainWindow/MainWindow.java
+++ src/databasegui/MainWindow/MainWindow.java
@@ -1,23 +1,20 @@
 package databasegui.MainWindow;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainWindow extends Application{
-    @Override
-    public void start(Stage primaryStage) throws Exception{
+public class MainWindow {
+
+    public Stage stage = new Stage();
+
+    public void launch() throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
-        primaryStage.setTitle("BlueCross BlueShield");
+        stage.setTitle("BlueCross BlueShield");
         Scene scene = new Scene(root, 500, 450);
         //scene.getStylesheets().add("databasegui/LoginWindow/LoginWindow.css");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void launchApp(){
-        launch();
+        stage.setScene(scene);
+        stage.show();
     }
 }
